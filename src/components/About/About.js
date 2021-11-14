846cf1d9fa0a2486d5f95275d3a2bede4bc26d32
mdsymoon <mdsymoon11@gmail.com>
2021-11-13 18:54:36 -0800

    about section complete

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import aboutImg from '../../images/about.svg';
 
 const About = () => {
     return (
-        <main className="About">
-            <div className="row">
-                <div className="col-md-6">
-                    
+        <main className="About container mt-5 pt-5">
+            <div className="row ">
+                <div className="col-sm-6">
+                    <h3 className="text-center mb-5">We Call Them <span className="fw-bold">Folio</span></h3>
+                    <p className="fs-3">For us Folio is a pdf file which has simple and concise written content, that would help the reader to gain knowledge quickly.</p>
+                    <p className="fs-3">An informative document on a certain topic, notes of a subject or any information in textual form is a folio.</p>
                 </div>
-                <div className="col-md-6">
-                    <img src="" alt="" />
+                <div className="col-sm-6 d-flex">
+                    <img src={aboutImg} alt="" />
                 </div>
             </div>
         </main>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import TopSection from '../TopSection/TopSection';
+import About from './../About/About';
 
 const Home = () => {
     return (
         <div>
             <Header/>
             <TopSection/>
+            <About/>
         </div>
     );
 };
